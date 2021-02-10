9411b218a579abd25b0d82187bafda11fc74e96b
jhonlinux <jhonathandrs>
2021-02-10 16:55:26 -0300

    removido o retorno do put

--- dsw/ambiente_aprendizagem/projetos/backend/src/main/java/com/jhonathanreis/backend/PessoaRest.java
+++ dsw/ambiente_aprendizagem/projetos/backend/src/main/java/com/jhonathanreis/backend/PessoaRest.java
@@ -33,8 +33,8 @@ public class PessoaRest {
 		pessoaDao.delete(pessoa);
 	}
 	
-	@PutMapping // Retorno add
-	public Pessoa put(@RequestBody Pessoa pessoa) {
-		return pessoaDao.save(pessoa);
+	@PutMapping 
+	public void put(@RequestBody Pessoa pessoa) {
+		pessoaDao.save(pessoa);
 	}
 }
